469402ece852e29babb0e5b891ba1e0950d6ecc4
C0uler <jordanyoh2@gmail.com>
2024-05-18 02:37:49 +0700

    benerin navbar

--- src/Home.jsx
+++ src/Home.jsx
@@ -158,7 +158,7 @@ export default function StickyNavbar() {
   return (
     <>
     
-      <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 py-2 lg:px-8 lg:py-4 navbar">
+      <Navbar className="sticky top-0 z-10 h-max max-w-full rounded-none px-4 lg:px-8 lg:py-1 navbar">
         <div className="flex items-center justify-between text-blue-gray-900">
         <Typography
           as="a"
