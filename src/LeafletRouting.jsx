2f371124c2a324935703617f4174bd9fb3dd2003
C0uler <jordanyoh2@gmail.com>
2024-05-18 15:28:20 +0700

    first commit

--- src/LeafletRouting.jsx
+++ src/LeafletRouting.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useMap } from 'react-leaflet';
 import L, { popup } from 'leaflet';
-import 'leaflet-routing-machine';
+import 'leaflet-routing-machine/dist/leaflet-routing-machine.js';
 import 'leaflet-routing-machine/dist/leaflet-routing-machine.css';
 import 'leaflet-control-geocoder/dist/Control.Geocoder.js';
 
